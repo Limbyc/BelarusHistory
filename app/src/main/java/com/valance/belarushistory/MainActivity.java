9c069ff870ee45bccbfdd64d604781919e2f6a8f
Limbyc <slavaloi582@gmail.com>
2023-02-08 14:15:52 +0300

    Initial commit

--- app/src/main/java/com/valance/belarushistory/MainActivity.java
+++ app/src/main/java/com/valance/belarushistory/MainActivity.java
@@ -10,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+<<<<<<< HEAD
+=======
+        System.out.println("hello");
+>>>>>>> e2600c4 (Initial commit)
     }
 }
